30886cf32ea90eb17c1496d1630e39b9c6d19c9c
ThePlayerRolo <thesolis78243@gmail.com>
2026-05-01 09:57:24 -0400

    fix inverted normals

--- Scripts/MeshGeneration/CreateSpecialMesh.gd
+++ Scripts/MeshGeneration/CreateSpecialMesh.gd
@@ -41,68 +41,76 @@ static func createSurfaceData(fileName: String) -> Part_SurfaceData:
 	
 	meshData = meshData.erase(0, firstBracket)
 	
-	for i in faceCount * 3:
-		var pos:int = meshData.find("[")
-		var vertPosition:Vector3;
-		var vertNormals:Vector3;
-		var vertTexCoords:Vector2;
+	for i in faceCount:
+		var vertsPositions:PackedVector3Array
+		var vertsNormals:PackedVector3Array
+		var vertsTexcoords:PackedVector2Array
+		for j in 3:
+			var pos:int = meshData.find("[")
+			var vertPosition:Vector3;
+			var vertNormals:Vector3;
+			var vertTexCoords:Vector2;
 
-		meshData = meshData.erase(0, pos+1)
-		
-		# X Position
-		pos = meshData.find(",")
-		vertPosition.x = float(meshData.substr(0, pos))
-		meshData = meshData.erase(0, pos+1)
-		
-		# Y Position
-		pos = meshData.find(",")
-		vertPosition.y = float(meshData.substr(0, pos))
-		meshData = meshData.erase(0, pos+1)
-		
-		# Z Position
-		pos = meshData.find(",")
-		vertPosition.z = float(meshData.substr(0, pos))
-		
-		pos = meshData.find("[")
-		
-		meshData = meshData.erase(0, pos+1)
-		# X Normal
-		pos = meshData.find(",")
-		vertNormals.x = float(meshData.substr(0, pos))
-		print(vertNormals.x)
-		meshData = meshData.erase(0, pos+1)
-		
-		# Y Normal
-		pos = meshData.find(",")
-		vertNormals.y = float(meshData.substr(0, pos))
-		print(vertNormals.y)
-		meshData = meshData.erase(0, pos+1)
-		
-		# Z Normal
-		pos = meshData.find(",")
-		vertNormals.z = float(meshData.substr(0, pos))
-		print(vertNormals.z)
-		
-		pos = meshData.find("[")
-		meshData = meshData.erase(0, pos+1)
-		
-		# X Tex
-		pos = meshData.find(",")
-		vertTexCoords.x = float(meshData.substr(0, pos))
-		meshData = meshData.erase(0, pos+1)
-		
-		# Y Tex
-		pos = meshData.find(",")
-		
-		vertTexCoords.y = 1 - float(meshData.substr(0, pos))
-		meshData = meshData.erase(0, pos+1)
-		pos = meshData.find("]")
+			meshData = meshData.erase(0, pos+1)
+			
+			# X Position
+			pos = meshData.find(",")
+			vertPosition.x = float(meshData.substr(0, pos))
+			meshData = meshData.erase(0, pos+1)
+			
+			# Y Position
+			pos = meshData.find(",")
+			vertPosition.y = float(meshData.substr(0, pos))
+			meshData = meshData.erase(0, pos+1)
+			
+			# Z Position
+			pos = meshData.find(",")
+			vertPosition.z = float(meshData.substr(0, pos))
+			
+			pos = meshData.find("[")
+			
+			meshData = meshData.erase(0, pos+1)
+			# X Normal
+			pos = meshData.find(",")
+			vertNormals.x = float(meshData.substr(0, pos))
+			meshData = meshData.erase(0, pos+1)
+			
+			# Y Normal
+			pos = meshData.find(",")
+			vertNormals.y = float(meshData.substr(0, pos))
+			meshData = meshData.erase(0, pos+1)
+			
+			# Z Normal
+			pos = meshData.find(",")
+			vertNormals.z = float(meshData.substr(0, pos))
+			
+			pos = meshData.find("[")
+			meshData = meshData.erase(0, pos+1)
+			
+			# X Tex
+			pos = meshData.find(",")
+			vertTexCoords.x = float(meshData.substr(0, pos))
+			meshData = meshData.erase(0, pos+1)
+			
+			# Y Tex
+			pos = meshData.find(",")
+			
+			vertTexCoords.y = 1 - float(meshData.substr(0, pos))
+			meshData = meshData.erase(0, pos+1)
+			pos = meshData.find("]")
 
-		meshData = meshData.erase(0, pos+1)
-		
-		if version == SPECIAL_MESH_VERSIONS.VERSION_1:
-			vertPosition *= 0.5
-		surfaceData.appendVertexData(vertPosition, flipNormals(vertNormals), vertTexCoords)
-	
+			meshData = meshData.erase(0, pos+1)
+			
+			if version == SPECIAL_MESH_VERSIONS.VERSION_1:
+				vertPosition *= 0.5
+			vertsPositions.append(vertPosition)
+			vertsNormals.append(vertNormals)
+			vertsTexcoords.append(vertTexCoords)
+		surfaceData.appendVertexData(vertsPositions[0], vertsNormals[0], vertsTexcoords[0])
+		surfaceData.appendVertexData(vertsPositions[2], vertsNormals[2], vertsTexcoords[2])
+		surfaceData.appendVertexData(vertsPositions[1], vertsNormals[1], vertsTexcoords[1])
+		vertsPositions.clear()
+		vertsNormals.clear()
+		vertsTexcoords.clear()
 	
 	return surfaceData;
--- Scripts/Part/Part.gd
+++ Scripts/Part/Part.gd
@@ -2,11 +2,11 @@ extends Node3D
 
 
 @export var meshType : Part_Enums.MESH_TYPE = Part_Enums.MESH_TYPE.MESH_SPECIAL
-@onready var material: StandardMaterial3D = preload("res://Resources/Shader/OldRobloxTexture.tres")
+@onready var material: StandardMaterial3D = preload("res://Resources/Shader/OldRobloxTextureSword.tres")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var mesh = MeshInstance3D.new()
-	var surfaceData:Part_SurfaceData = CreateSpecialMesh.createSurfaceData("res://Models//MeshType//Chessboard.mesh")
+	var surfaceData:Part_SurfaceData = CreateSpecialMesh.createSurfaceData("res://Models//MeshType//Sword.mesh")
 	mesh.mesh = surfaceData.createArrayMesh()
 	add_child(mesh)
 	mesh.set_surface_override_material(0, material)
